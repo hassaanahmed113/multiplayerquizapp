a665ac7e233cd1b6dedb7b712d66e7da2d1196bf
HassaanAhmed60211 <skhassaan113@gmail.com>
2023-10-31 21:53:18 +0500

    multiplayerappv2

---
+++ lib/Model/RoomModel.dart
@@ -0,0 +1,28 @@
+class RoomModel {
+  String currentUserid;
+  String opponentUserid;
+  int currentuserCorrect;
+  int currentuserWrong;
+  int opponentuserCorrect;
+  int opponentuserWrong;
+
+  RoomModel({
+    required this.currentUserid,
+    required this.opponentUserid,
+    required this.currentuserCorrect,
+    required this.currentuserWrong,
+    required this.opponentuserCorrect,
+    required this.opponentuserWrong,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'currentUserid': currentUserid,
+      'opponentUserid': opponentUserid,
+      'currentuserCorrect': currentuserCorrect,
+      'currentuserWrong': currentuserWrong,
+      'opponentuserCorrect': opponentuserCorrect,
+      'opponentuserWrong': opponentuserWrong,
+    };
+  }
+}
